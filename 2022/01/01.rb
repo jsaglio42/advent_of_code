a85d68cbbf53d5933ffe8c2e169c250cf3d9b7d0
Jean Saglio <jean.saglio@doctolib.com>
2022-12-01 17:34:30 +0100

    First day

---
+++ 2022/01/01.rb
@@ -0,0 +1,21 @@
+calories_by_elf = []
+
+calories = []
+File.open('d01.input.txt').each do |line|
+  if line.strip.empty?
+    calories_by_elf.push(calories)
+    calories = []
+  else
+    calories.push(line.to_i)
+  end
+end
+
+total_calories_by_elf = calories_by_elf.map(&:sum)
+
+puts "calories_by_elf:#{calories_by_elf}"
+puts "total_calories_by_elf:#{total_calories_by_elf}"
+# Answer 1
+puts total_calories_by_elf.max
+puts '---'
+# Answer 2
+puts total_calories_by_elf.sort.reverse[0..2].sum
