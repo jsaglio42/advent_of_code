b4356a75a35232da6de6019ebb2acd3485cae931
Jean Saglio <jean.saglio@doctolib.com>
2022-12-03 08:15:52 +0100

    Small changes

--- 2022/01/01.rb
+++ 2022/01/01.rb
@@ -1,21 +1,22 @@
-calories_by_elf = []
+# frozen-string-literal: true
 
+calories_by_elf = []
 calories = []
-File.open('input.txt').each do |line|
-  if line.strip.empty?
-    calories_by_elf.push(calories)
-    calories = []
-  else
-    calories.push(line.to_i)
+
+File
+  .open('input.txt')
+  .each do |line|
+    if line.strip.empty?
+      calories_by_elf.push(calories)
+      calories = []
+    else
+      calories.push(line.to_i)
+    end
   end
-end
 
 total_calories_by_elf = calories_by_elf.map(&:sum)
 
-puts "calories_by_elf:#{calories_by_elf}"
-puts "total_calories_by_elf:#{total_calories_by_elf}"
 # Answer 1
 puts total_calories_by_elf.max
-puts '---'
 # Answer 2
 puts total_calories_by_elf.sort.reverse[0..2].sum
--- 2022/02/02.rb
+++ 2022/02/02.rb
@@ -1,4 +1,4 @@
-
+# frozen-string-literal: true
 
 PAPER = 'PAPER'
 ROCK = 'ROCK'
@@ -8,45 +8,38 @@ LOSS = 0
 DRAW = 3
 WIN = 6
 
-CHOICE_TO_MOVE = {
-  "A" => ROCK,
-  "B" => PAPER,
-  "C" => SCISSOR,
-  "X" => ROCK,
-  "Y" => PAPER,
-  "Z" => SCISSOR,
-}
+CHOICE_TO_MOVE = { 'A' => ROCK, 'B' => PAPER, 'C' => SCISSOR, 'X' => ROCK, 'Y' => PAPER, 'Z' => SCISSOR }.freeze
 
-CHOICE_TO_OUTCOME = {
-  "X" => LOSS,
-  "Y" => DRAW,
-  "Z" => WIN,
-}
+CHOICE_TO_OUTCOME = { 'X' => LOSS, 'Y' => DRAW, 'Z' => WIN }.freeze
 
 OUTCOME_MAPPING = {
   ROCK => {
-      ROCK => DRAW,
-      PAPER => WIN,
-      SCISSOR => LOSS,
+    ROCK => DRAW,
+    PAPER => WIN,
+    SCISSOR => LOSS,
   },
   PAPER => {
-      ROCK => LOSS,
-      PAPER => DRAW,
-      SCISSOR => WIN,
+    ROCK => LOSS,
+    PAPER => DRAW,
+    SCISSOR => WIN,
   },
   SCISSOR => {
-      ROCK => WIN,
-      PAPER => LOSS,
-      SCISSOR => DRAW,
+    ROCK => WIN,
+    PAPER => LOSS,
+    SCISSOR => DRAW,
   },
 }
 
 def move_score(move)
   case move
-    when ROCK then 1
-    when PAPER then 2
-    when SCISSOR then 3
-    else 0
+  when ROCK
+    1
+  when PAPER
+    2
+  when SCISSOR
+    3
+  else
+    0
   end
 end
 
@@ -55,27 +48,29 @@ def match_score(opponent, me)
 end
 
 scores = []
-File.open('input.txt').each do |line|
-  opponent_move, my_move = line.split.map{ |choice| CHOICE_TO_MOVE[choice] }
-  scores.push(match_score(opponent_move, my_move))
-end
+File
+  .open('input.txt')
+  .each do |line|
+    opponent_move, my_move = line.split.map { |choice| CHOICE_TO_MOVE[choice] }
+    scores.push(match_score(opponent_move, my_move))
+  end
 
 puts scores.sum
 
 def find_move(opponent_move, outcome)
-  move, score = OUTCOME_MAPPING[opponent_move].find { |k, v| v == outcome}
-  return move
+  move, = OUTCOME_MAPPING[opponent_move].find { |_, v| v == outcome }
+  move
 end
-
 
 scores = []
-File.open('input.txt').each do |line|
-  opponent_choice, outcome = line.split
-  opponent_move = CHOICE_TO_MOVE[opponent_choice]
-  outcome = CHOICE_TO_OUTCOME[outcome]
-  my_move = find_move(opponent_move, outcome)
-  scores.push(match_score(opponent_move, my_move))
-end
+File
+  .open('input.txt')
+  .each do |line|
+    opponent_choice, outcome = line.split
+    opponent_move = CHOICE_TO_MOVE[opponent_choice]
+    outcome = CHOICE_TO_OUTCOME[outcome]
+    my_move = find_move(opponent_move, outcome)
+    scores.push(match_score(opponent_move, my_move))
+  end
 
 puts scores.sum
-
