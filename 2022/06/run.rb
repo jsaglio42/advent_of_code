273bb9714811f77eaa5515a5fa63e3eb1c293f45
Jean Saglio <jean.saglio@doctolib.com>
2022-12-06 13:08:03 +0100

    minor rework

--- 2022/06/run.rb
+++ 2022/06/run.rb
@@ -2,25 +2,16 @@
 
 file = File.read('input.txt').split("\n")
 
-NUMBER_TO_PROCESS = 4
+def start_of_input?(chars, index, size)
+  chars.slice(index, size).uniq.size == size
+end
 
-def start_of_input?(chars, index)
-  chars.slice(index, NUMBER_TO_PROCESS).uniq.size == NUMBER_TO_PROCESS
+def find_total_processed(line, size)
+  line.chars.find_index.with_index { |_, index| start_of_input?(line.chars, index, size) } + size
 end
 
 # Answer 1
-pp(
-  file.map do |line|
-    chars = line.chars
-    chars.find_index.with_index { |_, index| start_of_input?(chars, index) } + NUMBER_TO_PROCESS
-  end,
-)
+pp(file.map { |line| find_total_processed(line, 4) })
 
 # Answer 2
-NUMBER_TO_PROCESS = 14
-pp(
-  file.map do |line|
-    chars = line.chars
-    chars.find_index.with_index { |_, index| start_of_input?(chars, index) } + NUMBER_TO_PROCESS
-  end,
-)
+pp(file.map { |line| find_total_processed(line, 14) })
