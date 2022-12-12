29a6aec159c53242a84f5a9aa2d7543b3500f4c5
Jean Saglio <jean.saglio@doctolib.com>
2022-12-12 23:02:58 +0100

    Cleanup

--- 2022/12/run.rb
+++ 2022/12/run.rb
@@ -24,43 +24,27 @@ finish_j = MAP[finish_i].find_index { |char| char == char_to_height('E') }
 MAP[finish_i][finish_j] = 25
 FINISH = Node.new([finish_i, finish_j], nil)
 
-visited_nodes = []
-nodes_to_visit = [START]
+def bfs(starting_node, neighbours_proc, breaking_proc)
+  visited_nodes = []
+  nodes_to_visit = [starting_node]
+  while nodes_to_visit.size > 0
+    current = nodes_to_visit.shift()
+    visited_nodes.push(current)
 
-def neighbours(current_node, useless_nodes)
-  current_i, current_j = current_node.coord
-  possible_coord = [
-    [current_i - 1, current_j],
-    [current_i + 1, current_j],
-    [current_i, current_j - 1],
-    [current_i, current_j + 1],
-  ]
-  possible_coord.reject do |i, j|
-    next true if i < 0 || j < 0
-    next true if i > MAX_I || j > MAX_J
-    next true if MAP[i][j] - MAP[current_i][current_j] > 1
-    next true if useless_nodes.map(&:coord).include?([i, j])
+    break if breaking_proc.call(current)
 
-    false
+    available_nodes = neighbours_proc.call(current, visited_nodes + nodes_to_visit)
+    available_nodes.each { |i, j| nodes_to_visit.push(Node.new([i, j], current)) }
   end
+  raise 'no solution' if !breaking_proc.call(visited_nodes.last)
+  calculate_path(visited_nodes.last)
 end
 
-while nodes_to_visit.size > 0
-  current = nodes_to_visit.shift()
-  visited_nodes.push(current)
-  break if current == FINISH
-
-  available_nodes = neighbours(current, visited_nodes + nodes_to_visit)
-  available_nodes.each { |i, j| nodes_to_visit.push(Node.new([i, j], current)) }
-end
-raise 'no solution' if visited_nodes.last != FINISH
-
-# Answer 1
-def path(last_node)
+def calculate_path(last_node)
   [].tap do |path|
     it = last_node
     while it
-      path.unshift(it)
+      path.unshift(it.coord)
       it = it.parent
     end
   end
@@ -68,12 +52,62 @@ end
 
 def draw(path)
   output = Array.new(MAX_I + 1) { Array.new(MAX_J + 1) { '.' } }
-  path.each do |node|
-    i, j = node.coord
-    output[i][j] = '#'
-  end
+  path.each { |i, j| output[i][j] = '#' }
   output.each { |line| puts line.join }
 end
 
-pp path(visited_nodes.last).size - 1
-draw(path(visited_nodes.last))
+# Answer 1
+neighbours_proc =
+  Proc.new do |current_node, useless_nodes|
+    current_i, current_j = current_node.coord
+    possible_coord = [
+      [current_i - 1, current_j],
+      [current_i + 1, current_j],
+      [current_i, current_j - 1],
+      [current_i, current_j + 1],
+    ]
+    possible_coord.reject do |i, j|
+      next true if i < 0 || j < 0
+      next true if i > MAX_I || j > MAX_J
+      next true if MAP[i][j] - MAP[current_i][current_j] > 1
+      next true if useless_nodes.map(&:coord).include?([i, j])
+
+      false
+    end
+  end
+
+breaking_proc = Proc.new { |node| node == FINISH }
+
+path = bfs(START, neighbours_proc, breaking_proc)
+pp path.size - 1
+draw(path)
+
+# Answer 2
+neighbours_proc =
+  Proc.new do |current_node, useless_nodes|
+    current_i, current_j = current_node.coord
+    possible_coord = [
+      [current_i - 1, current_j],
+      [current_i + 1, current_j],
+      [current_i, current_j - 1],
+      [current_i, current_j + 1],
+    ]
+    possible_coord.reject do |i, j|
+      next true if i < 0 || j < 0
+      next true if i > MAX_I || j > MAX_J
+      next true if MAP[i][j] - MAP[current_i][current_j] < -1
+      next true if useless_nodes.map(&:coord).include?([i, j])
+
+      false
+    end
+  end
+
+breaking_proc =
+  Proc.new do |node|
+    i, j = node.coord
+    MAP[i][j] == 0
+  end
+
+path = bfs(FINISH, neighbours_proc, breaking_proc)
+pp path.size - 1
+draw(path)
