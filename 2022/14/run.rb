996bc1534d8808ad81d365183d8befa010cfe107
Jean Saglio <jean.saglio@doctolib.com>
2022-12-23 19:25:02 +0100

    some cleanup

--- 2022/14/run.rb
+++ 2022/14/run.rb
@@ -2,7 +2,7 @@
 
 require 'matrix'
 
-FILE_NAME = 'sample.txt'
+FILE_NAME = 'input.txt'
 
 input =
   File
@@ -18,108 +18,105 @@ input =
         end
     end
 
-SOURCE = Vector[0, 500]
+SOURCE = [0, 500]
+
 ROCKS =
-  input.each_with_object([]) do |edges, acc|
-    (0...edges.size - 1).each do |edge_index|
-      start = edges[edge_index]
-      stop = edges[edge_index + 1]
-      diff = stop - start
-      (0...diff.magnitude).each { |length| acc << (start + (length * diff.normalize)).map(&:to_i) }
+  input
+    .each_with_object([]) do |edges, acc|
+      (0...edges.size - 1).each do |edge_index|
+        start = edges[edge_index]
+        stop = edges[edge_index + 1]
+        diff = stop - start
+        (0...diff.magnitude).each { |length| acc << (start + (length * diff.normalize)).map(&:to_i) }
+      end
+      acc << edges.last
     end
-    acc << edges.last
+    .map(&:to_a)
+
+
+
+def rock?(tiles, position)
+  tiles[position] == '#' || position[0] == rock_bottom + 2
+end
+
+def sand?(tiles, position)
+  tiles[position] == 'o'
+end
+
+def empty?(tiles, position)
+  !rock?(tiles, position) && !sand?(tiles, position)
+end
+
+def out_of_boundary?(position)
+  position[0] > rock_bottom
+end
+
+def rock_bottom
+  @rock_bottom ||= ROCKS.map(&:first).max
+end
+
+def new_tiles
+  ROCKS.to_h { |rock| [rock, '#'] }
+end
+
+def draw(tiles)
+  min_i, min_j = [0, 500 - rock_bottom - 2]
+  max_i, max_j = [rock_bottom + 2, 500 + rock_bottom + 2]
+
+  (min_i..max_i).each do |i|
+    if i == rock_bottom + 2
+      line = '#' * (max_j - min_j + 1)
+      puts line
+    else
+      line = '.' * (max_j - min_j + 1)
+      tiles.select { |position| i == position[0] }.each { |position, char| line[position[1] - min_j] = char }
+      puts line
+    end
   end
+end
 
-SANDS = []
-
-def boundary(positions, direction, method)
-  case direction
-  when :i
-    positions.map(&:to_a).map(&:first).send(method)
-  when :j
-    positions.map(&:to_a).map(&:last).send(method)
+def next_position(tiles, position)
+  i, j = position
+  [[1, 0], [1, -1], [1, 1]].each do |offset_i, offset_j|
+    return [i + offset_i, j + offset_j] if empty?(tiles, [i + offset_i, j + offset_j])
   end
+  position
 end
 
-def source?(vector)
-  Vector[0, 500] == vector
-end
-
-def rock?(vector)
-  ROCKS.include?(vector) || vector[0] == boundary(ROCKS, :i, :max) + 2
-end
-
-def sand?(vector)
-  SANDS.include?(vector)
-end
-
-def empty?(vector)
-  !rock?(vector) && !sand?(vector)
-end
-
-def out_of_boundary?(vector)
-  vector[0] > boundary(ROCKS, :i, :max)
-end
-
-def safe_method(a, b, method)
-  [a, b || a].send(method)
-end
-
-def draw
-  min_i = safe_method(boundary(ROCKS, :i, :min), boundary(SANDS, :i, :min), :min) - 1
-  max_i = safe_method(boundary(ROCKS, :i, :max), boundary(SANDS, :i, :max), :max) + 1
-  min_j = safe_method(boundary(ROCKS, :j, :min), boundary(SANDS, :j, :min), :min) - 1
-  max_j = safe_method(boundary(ROCKS, :j, :max), boundary(SANDS, :j, :max), :max) + 1
-
-  height = max_i - min_i + 1
-  width = max_j - min_j + 1
-
-  map = '.' * (height * width)
-  ROCKS.each { |node| map[((node[0] - min_i) * width) + (node[1] - min_j)] = '#' }
-  SANDS.each { |node| map[((node[0] - min_i) * width) + (node[1] - min_j)] = 'o' }
-
-  height.times { |idx| puts map.slice(idx * width, width) }
-  puts "\n"
-end
-
-def next_position(vector)
-  [Vector[1, 0], Vector[1, -1], Vector[1, 1]].each { |offset| return vector + offset if empty?(vector + offset) }
-  vector
-end
-
-def stopped?(vector)
-  next_position(vector) == vector
+def stopped?(tiles, position)
+  next_position(tiles, position) == position
 end
 
 class OutOfBoundary < StandardError
 end
 
-def fill(raise_if_oob: true)
+def fill(tiles, raise_if_oob: true)
+  # counter = 0
   sources = [SOURCE]
   while !sources.empty?
-    if !empty?(sources.last)
-      sources.pop
-      next
-    end
+    next sources.pop if !empty?(tiles, sources.last)
     it = sources.last
-    until (stopped?(it) || out_of_boundary?(it))
-      it = next_position(it)
-      sources << it
+    until (stopped?(tiles, it))
+      it = next_position(tiles, it)
+      raise OutOfBoundary if raise_if_oob && out_of_boundary?(it)
+      sources.push(it)
     end
-    raise OutOfBoundary if raise_if_oob && out_of_boundary?(next_position(it))
-    SANDS << it
+    tiles[it] = 'o'
+    # draw(tiles) if (counter += 1) % 1000 == 0
   end
 end
 
-# Answer 1
+tiles = new_tiles
 begin
-  fill(raise_if_oob: true)
+  fill(tiles, raise_if_oob: true)
 rescue OutOfBoundary
-  pp SANDS.count
+  nil
 end
-draw
+draw(tiles)
+pp(tiles.values.count { |char| char == 'o' })
 
 # Answer 2
-fill(raise_if_oob: false)
-pp SANDS.count
-draw
+tiles = new_tiles
+fill(tiles, raise_if_oob: false)
+draw(tiles)
+pp(tiles.values.count { |char| char == 'o' })
